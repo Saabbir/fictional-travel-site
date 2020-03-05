a3d080e631b4248bad670fb99967eca435529002
Saabbir Hossain <thesaabbir@gmail.com>
2020-03-05 17:32:02 +0600

    Create sticky header

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,8 +1,10 @@
 import '../styles/index.postcss'
 import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
 import RevealOnScroll from './modules/RevealOnScroll'
 
 new MobileMenu()
+new StickyHeader()
 new RevealOnScroll('.feature-item', 75)
 new RevealOnScroll('.testimonial', 60)
 
