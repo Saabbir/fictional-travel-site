5a482e03300fe5a72cb70b7268e19165bd67063f
Saabbir Hossain <thesaabbir@gmail.com>
2020-03-06 06:22:50 +0600

    Configure webpack for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
 const postCSSPlugins = [
   require('postcss-import'),
@@ -9,13 +14,48 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
+const cssConfig = {
+  test: /\.(css|pcss|postcss)$/i,
+  use: [ 
+    'css-loader',
+    {
+      loader: 'postcss-loader',
+      options: {
+        plugins: postCSSPlugins
+      }
+    }
+  ]
+}
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy images', function() {
+      fse.copySync('./app/assets/images', './docs/assets/images')
+    })
+  }
+}
+
+let config = {
   entry: './app/assets/scripts/index.js',
-  output: {
+  module: {
+    rules: [
+      cssConfig
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/index.html'
+    })
+  ]
+}
+
+if (currentTask === 'dev') {
+  config.output = {
     path: path.resolve(__dirname, 'app'),
     filename: 'bundle.js'
-  },
-  devServer: {
+  }
+  config.devServer = {
     before: function(app, server) {
       server._watch('./app/**/*.html')
     },
@@ -23,23 +63,41 @@ module.exports = {
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(css|pcss|postcss)$/i,
-        use: [
-          'style-loader', 
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: postCSSPlugins
-            }
-          }
-        ]
-      }
-    ]
   }
+  config.devtool = 'inline-source-map'
+  config.mode = 'development'
+  cssConfig.use.unshift('style-loader')
 }
+
+if (currentTask === 'build') {
+  config.output = {
+    path: path.resolve(__dirname, 'docs'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: { chunks: 'all' }
+  }
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  })
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require('cssnano'))
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[chunkhash].css'
+    }),
+    new RunAfterCompile()
+  )
+}
+
+module.exports = config
